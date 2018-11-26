04c764d57cbb8818459b7cd19d609cc1d6b9551b
davicorreiajr <davicorreiajr@gmail.com>
2018-11-27 00:18:20 +0200

    Fix getLatestVersion from gh datasource

--- src/js/github-datasource.js
+++ src/js/github-datasource.js
@@ -3,10 +3,12 @@ const fetch = require('electron-fetch').default;
 
 exports.getLatestVersion = function() {
   return fetch('https://api.github.com/repos/davicorreiajr/spotify-now-playing/releases/latest', {
-    method: 'GET'
+    method: 'GET',
+    headers: { 'Accept': 'application/vnd.github.v3+json' }
   })
     .then(res => res.json())
     .then(res => ({
-      version: res.name
+      version: res.name,
+      dmgDownloadUrl: res.assets[0].browser_download_url
     }));
 };
