a17590df8778f5a75afcf1753110f4737b20bd41
davicorreiajr <davicorreiajr@gmail.com>
2018-11-26 23:26:22 +0200

    Add base frontend of updater window

--- src/js/updater.js
+++ src/js/updater.js
@@ -1,10 +1,36 @@
 'use strict';
-const { app } = require('electron');
+const { app, BrowserWindow } = require('electron');
 const githubDatasource = require('./github-datasource');
 
-exports.execute = function() {
+function createWindow(window) {
+  return new BrowserWindow(
+    {
+      parent: window,
+      width: 500,
+      height: 250,
+      modal: true,
+      show: true,
+      alwaysOnTop: true,
+      webPreferences: {
+        devTools: true // REMOVE
+      }
+    }
+  );
+}
+
+function isAppUpdated(versionFromGithub) {
+  const localAppVersion = app.getVersion();
+  return localAppVersion === versionFromGithub;
+}
+
+function showUpdateWindow(parentWindow) {
+  const updateWindow = createWindow(parentWindow);
+  updateWindow.loadFile('src/html/update.html');
+}
+
+exports.execute = function(parentWindow) {
   githubDatasource.getLatestVersion()
-    .then(data => console.log('latest version ', data));
-  
-  console.log('appVersion ', app.getVersion());
+    .then(data => {
+      if(!isAppUpdated(data.version)) showUpdateWindow(parentWindow);
+    });
 };
