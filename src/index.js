707172a9033333751e834c3ba4b9ffbbd17cb1e1
davicorreiajr <davicorreiajr@gmail.com>
2018-11-27 03:02:05 +0200

    Add copy option

--- src/index.js
+++ src/index.js
@@ -86,16 +86,9 @@ function setApplicationMenuToEnablePaste() {
     {
       label: 'Edit',
       submenu: [
-        {
-          label: 'Paste',
-          accelerator: 'CmdOrCtrl+V',
-          selector: 'paste:'
-        },
-        {
-          label: 'Select All',
-          accelerator: 'CmdOrCtrl+A',
-          selector: 'selectAll:'
-        }
+        { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+        { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+        { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
       ]
     }
   ];
