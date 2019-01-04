cfe14978a47a1802bce1373e4d9489c11901e262
davicorreiajr <davicorreiajr@gmail.com>
2019-01-04 20:09:27 +0200

    Add user and tag (country) to sentry-config

--- src/helpers/sentry-config.js
+++ src/helpers/sentry-config.js
@@ -1,10 +1,9 @@
 'use strict';
 const Sentry = require('@sentry/electron');
 
-exports.execute = function() {
+exports.execute = function(user) {
   Sentry.configureScope((scope) => {
-    scope.setUser({
-      id: "john.doe@example.com"
-    });
+    scope.setUser({ id: user.uri });
+    scope.setTag('user_country', user.country);
   });
 };
