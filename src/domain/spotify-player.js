a8760f8bf8848e2f6dd0ec7a98d7ebfe6700f7b3
davicorreiajr <davicorreiajr@gmail.com>
2019-01-04 17:48:06 +0200

    Remove auth methods from spotify-player and never stop the loop

--- src/domain/spotify-player.js
+++ src/domain/spotify-player.js
@@ -4,10 +4,10 @@ const { ipcMain } = require('electron');
 const localStorage = require('../data-source/local-storage');
 const spotifyDataSource = require('../data-source/spotify-datasource');
 const subjectFactory = require('../helpers/subject-factory');
-const windowFactory = require('../helpers/window-factory');
 const mappers = require('../helpers/mappers');
 const errorReporter = require('../helpers/error-reporter');
-const { UPDATE_PERIOD, SPOTIFY_CLIENT_ID, SPOTIFY_SCOPES, REDIRECT_URI } = require('../helpers/constants');
+const authorizer = require('./authorizer');
+const { UPDATE_PERIOD } = require('../helpers/constants');
 const notifier = require('node-notifier');
 
 ipcMain.on('shuffleButtonClicked', () => spotifyDataSource.shuffle(localStorage.get('accessToken'), true));
@@ -25,48 +25,31 @@ let currentPlaybackURI;
 
 exports.execute = function(parentWindow) {
   const subject = subjectFactory.get();
-  let updateLoop;
+  let playbackLoop;
 
-  subject.on('authCode', getTokenFromAuthCode);
-  subject.on('token', startUpdateLoop);
-  subject.on('currentPlaybackReceived', (data) => sendToRendererProcess('currentPlaybackReceived', data));
   subject.on('errorCurrentPlayback', handleErrorCurrentPlayback);
-  subject.on('errorTokenFromRefreshToken', getAuthorization);
-  subject.on('errorTokenFromAuthCode', getAuthorization);
 
   ipcMain.on('addToPlaylistButtonClicked', handleAddToPlaylistButtonClicked);
   ipcMain.on('playlistSelected', (event, data) => handlePlaylistSelected(data));
-
-  const accessToken = localStorage.get('accessToken');
-
-  if(accessToken && areSavedScopesEnough()) {
-    startUpdateLoop(accessToken);
-  } else {
-    getAuthorization();
-  }
-
-  function startUpdateLoop(accessToken) {
-    updateLoop = setInterval(() => getCurrentPlayback(accessToken), UPDATE_PERIOD);
-  }
-
-  function sendToRendererProcess(channel, data) {
-    parentWindow.webContents.send(channel, data);
-  }
 
   function shouldShowTrackNotification(data) {
     return data.currentlyPlayingType === 'track' && (data.uri !== currentPlaybackURI) && localStorage.get('activateNotifications');
   }
 
-  function getCurrentPlayback(accessToken) {
+  setInterval(() => getCurrentPlayback(), UPDATE_PERIOD);
+  
+  function getCurrentPlayback() {
+    const accessToken = localStorage.get('accessToken');
+
     spotifyDataSource.getCurrentPlayback(accessToken)
       .then(json => {
         if(json.item) {
           const mappedData = mappers.currentPlaybackToView(json);
-          subject.emit('currentPlaybackReceived', mappedData);  
           if(shouldShowTrackNotification(mappedData)) {
             currentPlaybackURI = mappedData.uri;
             notifier.notify(mappers.notificationData(json));
           }
+          sendToRendererProcess('currentPlaybackReceived', mappedData);
         } else {
           sendToRendererProcess('loading', {});
           subject.emit('errorCurrentPlayback', null);
@@ -78,81 +61,15 @@ exports.execute = function(parentWindow) {
       });
   }
 
+  function sendToRendererProcess(channel, data) {
+    parentWindow.webContents.send(channel, data);
+  }
+
   function handleErrorCurrentPlayback() {
-    if(updateLoop) clearInterval(updateLoop);
-    updateLoop = null;
+    if(playbackLoop) clearInterval(playbackLoop);
+    playbackLoop = null;
 
-    const refreshToken = localStorage.get('refreshToken');
-    
-    if(!refreshToken) getAuthorization();
-    getTokenFromRefreshToken(refreshToken);
-  }
-
-  function getAuthorization() {
-    const spotifyAuthWindow = windowFactory.get('auth', { parentWindow });
-    const spotifyAuthUrl = `https://accounts.spotify.com/en/authorize?client_id=${SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=${encodeURI(SPOTIFY_SCOPES)}`;
-    spotifyAuthWindow.loadURL(spotifyAuthUrl);
-
-    spotifyAuthWindow.once('ready-to-show', () => spotifyAuthWindow.show());
-    
-    const webContents = spotifyAuthWindow.webContents;
-    webContents.on('did-finish-load', () => {
-      const url = webContents.getURL();
-      const urlQueryParams = url.split('?')[1] || '';
-      const urlSearchParams = new URLSearchParams(urlQueryParams);
-      const code = urlSearchParams.get('code');
-
-      if(isDomainUrlRedirectUri(url.split('?')[0]) && code) {
-        spotifyAuthWindow.destroy();
-        
-        const authCode = code.split('#')[0];
-        subject.emit('authCode', authCode);
-      }
-    });
-  }
-
-  function getTokenFromAuthCode(authCode) {
-    const body = new URLSearchParams();
-    body.append('grant_type', 'authorization_code');
-    body.append('code', authCode);
-    body.append('redirect_uri', REDIRECT_URI);
-
-    spotifyDataSource.getToken(body)
-      .then(json => {
-        if(json.access_token) {
-          localStorage.save('accessToken', json.access_token);
-          localStorage.save('refreshToken', json.refresh_token);
-          localStorage.save('authorizedScopes', json.scope);
-
-          spotifyDataSource.getCurrentUser(json.access_token)
-            .then(user => {
-              localStorage.save('userUri', user.uri);
-              subject.emit('token', json.access_token);
-            })
-            .catch(error => errorReporter.emit('getSpotifyCurrentUser', error));
-        } else {
-          subject.emit('errorTokenFromAuthCode', null);
-        }
-      })
-      .catch(error => errorReporter.emit('getSpotifyTokenFromAuthCode', error));
-  }
-
-  function getTokenFromRefreshToken(refreshToken) {
-    const body = new URLSearchParams();
-    body.append('grant_type', 'refresh_token');
-    body.append('refresh_token', refreshToken);
-
-    spotifyDataSource.getToken(body)
-      .then(json => {
-        if(json.access_token) {
-          localStorage.save('accessToken', json.access_token);
-          if(json.refresh_token) localStorage.save('refreshToken', json.refresh_token);
-          subject.emit('token', json.access_token);
-        } else {
-          subject.emit('errorTokenFromRefreshToken', null);
-        }
-      })
-      .catch(error => errorReporter.emit('getSpotifyTokenFromRefreshToken', error));
+    authorizer.execute(parentWindow);
   }
 
   function handleAddToPlaylistButtonClicked() {
@@ -169,21 +86,7 @@ exports.execute = function(parentWindow) {
     const accessToken = localStorage.get('accessToken');
     const { playlistId, uri } = data;
     spotifyDataSource.addTrackToPlaylist(accessToken, playlistId, uri)
-      .then(response => response.error ? getAuthorization() : sendToRendererProcess('trackAdded'))
+      .then(response => response.error ? authorizer.execute(parentWindow) : sendToRendererProcess('trackAdded'))
       .catch(error => errorReporter.emit('addTrackToPlaylist', error));
-  }
-
-  function isDomainUrlRedirectUri(domainUrl) {
-    return domainUrl === REDIRECT_URI;
-  }
-  
-  function areSavedScopesEnough() {
-    const savedScopes = localStorage.get('authorizedScopes');
-    if(!savedScopes) return false;
-  
-    const savedScopesArray = savedScopes.split(' ');
-    const appScopesArray = SPOTIFY_SCOPES.split(' ');
-  
-    return appScopesArray.reduce((result, scope) => result && savedScopesArray.includes(scope), true);
   }
 };
